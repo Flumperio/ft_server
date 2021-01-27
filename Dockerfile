394000bb66b3956f7e26fa20a28d221e7aee65e0
Flumperio <juasanto@student.42madrid.com>
2021-01-27 13:19:09 +0100

    27 - Enero

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,19 @@
 FROM debian:buster
 WORKDIR /tmp
+EXPOSE 80
+EXPOSE 443
 
 RUN apt-get -q update
 RUN apt-get -q upgrade
 
 RUN apt-get install wget -yq
 #para comprobar que funciona - BORRAR -
+
 RUN apt-get install curl -yq
 
 #install nginx
 RUN apt-get install nginx -yq
+COPY ./srcs/nginx.conf /etc/nginx/sites-available/default
 COPY ./srcs/test.sh /tmp/test.sh
 RUN chmod 775 /tmp/test.sh
 CMD /tmp/test.sh
--- srcs/test.sh
+++ srcs/test.sh
@@ -1,3 +1,15 @@
-chmod 775 /tmp/test.sh
+
+
+#crear certificado
+mkdir /etc/nginx/ssl
+openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/nginx-selfsigned.pem -keyout /etc/ssl/certs/nginx-selfsigned.key -subj "/C=ES/ST=Madrid/L=Madrid/O=42Madrid/OU=juasanto/CN=default"
+
+
+#crear sitio www
+#mv /tmp/nginx.conf /etc/nginx/sites-available/default
+#ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+#rm -rf /etc/nginx/sites-enabled/default
+
+#arrancar NGINX
 service nginx start
 tail -f /var/log/nginx/access.log
